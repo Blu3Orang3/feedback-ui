ded341077a0c134294b46cd4d4a0263b67edc3d6
BlueOrang3 <matealtunashvili@gmial.com>
2022-09-12 22:53:55 +0400

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const feedbackData = [
+    {
+      id: 1,
+      rating: 10,
+      text: "This is feedback item 1 coming from the backend"
+    },
+    {
+      id: 2,
+      rating: 8,
+      text: "This is feedback item 2 coming from the backend"
+    },
+    {
+      id: 3,
+      rating: 10,
+      text: "This is feedback item 3 coming from the backend"
+    }
+]
+
+export default feedbackData;
+
